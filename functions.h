af743ec8f832b3ef5c7889836dd2f45f9d574462
Nikita <155087431+BuLbAsH2224@users.noreply.github.com>
2024-09-09 17:34:35 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,7 +3,7 @@
 
 char ArithmeticSigns[]{ '+' ,'-', '*', '/','^'};
 
-bool thisIsArithmetiÒSign(char &sign) {
+bool thisIsArithmeti—ÅSign(char &sign) {
     for (int i = 0; i < std::size(ArithmeticSigns); i++) {
         if (sign == ArithmeticSigns[i]) {
             return true;
@@ -16,7 +16,7 @@ auto ParsingAnExample(std::string& example) {
     std::vector<std::string> ParsingExample;
     std::string ValueBeforeSign;
     for (int i = 0; i < example.length(); i++) {
-            if (thisIsArithmetiÒSign(example[i]) == true && example[i-1] !=  '(') {
+            if (thisIsArithmeti—ÅSign(example[i]) == true && example[i-1] !=  '(') {
                 ParsingExample.push_back(ValueBeforeSign);
                 ParsingExample.push_back(std::string(1, example[i]));
                 ValueBeforeSign = "";
@@ -36,68 +36,85 @@ auto ParsingAnExample(std::string& example) {
 }
 
 
-float StringToFloat(std::string str) {
+double StringToDouble(std::string str) {
     std::string newStr = str;
-    float x = 0;
+    double x = 0;
     if (newStr.find("sin") != std::string::npos) {
         newStr = newStr.substr(4);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x = sin(std::stof(newStr));
+        x = sin(std::stod(newStr));
         return x;
     }
     else  if (newStr.find("cos") != std::string::npos) {
         newStr = newStr.substr(4);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x = cos(std::stof(newStr));
+        x = cos(std::stod(newStr));
         return x;
     }
     else  if (newStr.find("abs") != std::string::npos) {
         newStr = newStr.substr(4);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x = abs(std::stof(newStr));
+        x = abs(std::stod(newStr));
         return x;
     }
     else  if (newStr.find("sqrt") != std::string::npos) {
         newStr = newStr.substr(5);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x = sqrt(std::stof(newStr));
+        x = sqrt(std::stod(newStr));
         return x;
     }
     else  if (newStr.find("ceil") != std::string::npos) {
         newStr = newStr.substr(5);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x = ceil(std::stof(newStr));
+        x = ceil(std::stod(newStr));
         return x;
     }
     else  if (newStr.find("exp") != std::string::npos) {
         newStr = newStr.substr(4);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x = exp(std::stof(newStr));
+        x = exp(std::stod(newStr));
         return x;
     }
     else  if (newStr.find("log") != std::string::npos) {
         newStr = newStr.substr(4);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x = log(std::stof(newStr));
+        x = log(std::stod(newStr));
         return x;
     }
-
+    else  if (newStr.find("fac") != std::string::npos) {
+        newStr = newStr.substr(4);
+        newStr = newStr.substr(0, newStr.length() - 1);
+        x = std::stod(newStr);
+        double y = 1;
+        for (double i = 1; i <= x; i++) {
+                y = y * i;
+        }
+        return y;
+    }
+    else  if (newStr == "PI") {
+        x = 3.1415926535;
+        return x;
+    }
+    else  if (newStr == "(-PI)") {
+        x = 3.1415926535;
+        return x;
+    }
     else  if (newStr.find("(") != std::string::npos) {
         newStr = newStr.substr(1);
         newStr = newStr.substr(0, newStr.length() - 1);
-        x =  std::stof(newStr);
+        x =  std::stod(newStr);
         return x;
     }
     else {
-        x = std::stof(newStr);
+        x = std::stod(newStr);
         return x;
     }
     //std::stof(Hel)
     return 0;
 }
 
-std::string CalculateWithAction(float x, float y, std::string action) {
-    float answer = 0;
+std::string CalculateWithAction(double x, double y, std::string action) {
+    double answer = 0;
     std::string Str;
     if (action == std::string("+")) {
         answer = x + y;
@@ -115,7 +132,7 @@ std::string CalculateWithAction(float x, float y, std::string action) {
         answer = x / y;
     }
     Str = std::to_string(answer);
-    if (answer < float(0)) {
+    if (answer < double(0)) {
         Str.insert(0, std::string("("));
         Str.append(std::string(")"));
     }
@@ -123,8 +140,11 @@ std::string CalculateWithAction(float x, float y, std::string action) {
 }
 
 
-float calculate(std::vector<std::string> vect) {
+double calculate(std::vector<std::string> vect) {
     std::vector<std::string> vec = vect;
+    if (std::size(vec) == 1) {
+        return StringToDouble(vec[0]);
+    }
     while (true) {
         std::vector<std::string>::iterator ind = std::find(vec.begin(), vec.end(), "^");
         if (ind == vec.end()) {
@@ -143,8 +163,8 @@ float calculate(std::vector<std::string> vect) {
             std::string action = vec[findIndex];
 
           
-            float xFloat = StringToFloat(xString);
-            float yFloat = StringToFloat(yString);
+            double xDouble = StringToDouble(xString);
+            double yDouble = StringToDouble(yString);
          
                 vec.erase(vec.begin() + findIndex);
                 vec.erase(vec.begin() + findIndex);
@@ -157,7 +177,7 @@ float calculate(std::vector<std::string> vect) {
        //    std::cout <<  std::string(CalculateWithAction(xFloat, yFloat, action)) << std::endl;
        //    std::cout << xFloat << " " << yFloat << " " << action << std::endl;
            
-           std::string answer = std::string(CalculateWithAction(xFloat, yFloat, action));
+           std::string answer = std::string(CalculateWithAction(xDouble, yDouble, action));
 
            if (vec.empty()) {
            if (answer.find("(") != std::string::npos) {
@@ -165,7 +185,7 @@ float calculate(std::vector<std::string> vect) {
                answer = answer.substr(0, answer.length() - 1);
 
            }
-              return std::stof(answer);
+              return std::stod(answer);
            }
            vec.insert(vec.begin() + (findIndex - 1), answer);
         
